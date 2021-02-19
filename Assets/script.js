5409b7addee42e82116c278fb7cec43d86e79364
Dalton Wilkins <wilkins.271@osu.edu>
2021-02-18 19:01:16 -0500

    added functionality to multiple choice buttons and created initials prompt at end of quiz

--- Assets/script.js
+++ Assets/script.js
@@ -17,42 +17,105 @@ let answers = ["3", "3", "4", "4", "4"];
 let i = 0;
 let correct = 0;
 let incorrect = 0;
-
+let scores = [];
 
 let bold = document.querySelector("#bold");
 let content = document.querySelector("#content");
+let div = document.querySelector("div");
 let footer = document.querySelector("footer");
 let button = document.querySelector(".button");
+let submit = document.createElement("button");
+let input = document.createElement("input");
+let highScoresBtn = document.querySelector("#highscores");
 
-button.addEventListener("click", function() {
-    if (button.innerHTML.includes(answers[i])) {
-        correct++;
-        footer.innerHTML = "Correct!";
-        console.log(correct);
-    } else if (button.innerHTML.includes("1") || button.innerHTML.includes("2") || 
-    button.innerHTML.includes("3") || button.innerHTML.includes("4")) {
-        incorrect++;
-        footer.innerHTML = "Wrong!";
-        console.log(incorrect);
+function press() {
+        let result = document.createElement("p");
+        if (this.innerHTML !== "Start Quiz") {
+            if (this.innerHTML.includes(answers[i])) {
+                correct++;
+                result.innerHTML = "Correct!";
+            }
+            else {
+                incorrect++;
+                result.innerHTML = "Incorrect!";
+            }
+            i++;
+        }
+        footer.innerHTML = "";
+        footer.appendChild(result);
+
+        console.log(this.innerHTML);
+        console.log(answers[i]);
+        console.log("Correct: " + correct);
+        console.log("Incorrect: " + incorrect);
+
+    if (i < 5) {
+        
+
+        bold.innerHTML = "";
+        let question = document.createElement("h2");
+        question.innerHTML = fiveQuestions[i][0];
+        bold.appendChild(question);
+        console.log(question);
+
+        
+        div.innerHTML = "";
+        for (j = 1; j <= 4; j++) {
+            let one = document.createElement("button");
+            one.classList.add("button");
+            one.innerHTML = fiveQuestions[i][j];
+            div.appendChild(one);
+            one.addEventListener("click", press);
+        }
+        console.log(div);
     }
+    else {
+        bold.innerHTML = "";
+        let finish = document.createElement("h2");
+        finish.innerHTML = "All done!";
+        bold.appendChild(finish);
+        console.log(finish);
 
-    bold.innerHTML = "";
-    content.innerHTML = "";
-    let question = document.createElement("h2");
-    question.innerHTML = fiveQuestions[i][0];
-    bold.appendChild(question);
-    console.log(question);
+        div.innerHTML = "";
+        let score = document.createElement("h2");
+        score.innerHTML = "Your final score is 22.";
+        div.appendChild(score);
+        console.log(score);
 
-    let div = document.createElement("div");
-    div.innerHTML = "";
-    for (j = 1; j <= 4; j++) {
-        let one = document.createElement("button");
-        one.classList.add("button");
-        one.innerHTML = fiveQuestions[i][j];
-        div.appendChild(one);
+        let initials = document.createElement("h2");
+        initials.innerHTML = "Enter initials:";
+        div.appendChild(initials);
+        console.log(initials);
+
+        // <input type="text" class="form-input w-100" id="shopping-input" name="shopping-input" placeholder="Enter item name" />
+        
+        div.appendChild(input);
+        console.log(input);
+
+        
+        submit.classList.add("submit");
+        submit.innerHTML = "Submit";
+        div.appendChild(submit);
+        console.log(submit);
+
+        // footer.innerHTML = "";
+        result.innerHTML = "Correct: " + correct + " Incorrect: " + incorrect;
     }
-    content.appendChild(div);
-    console.log(div);
+}
 
-    i++;
-})
+function submitIt() {
+    let poop = input.value;
+    scores.push([poop, correct, incorrect]);
+    console.log(scores[scores.length - 1]);
+}
+
+function highScores() {
+    for (i in scores) {
+        console.log(scores[i]);
+    }
+}
+
+button.addEventListener("click", press);
+submit.addEventListener("click", submitIt);
+highScoresBtn.addEventListener("click", highScores);
+
