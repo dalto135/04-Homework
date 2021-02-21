d6c66f5f0ed6e02f2ab6d99823b259747a2deabb
Dalton Wilkins <wilkins.271@osu.edu>
2021-02-20 19:13:38 -0500

    cleaned up/deleted redundant code

--- Assets/script.js
+++ Assets/script.js
@@ -13,15 +13,13 @@ let fiveQuestions = [["Commonly used data types DO NOT include:", "1. strings", 
 // }
 // let keys = Object.keys(five);
 
-let answers = ["3", "3", "4", "4", "4"];
+
 let i = 0;
 let correct = 0;
 let incorrect = 0;
 let scores = [];
 
 let bold = document.querySelector("#bold");
-let boldTitle = document.querySelector("#bold h2");
-
 let content = document.querySelector("#content");
 let div = document.querySelector("div");
 let footer = document.querySelector("footer");
@@ -31,18 +29,15 @@ let highScoresBtn = document.querySelector("#highscores");
 let submit = document.createElement("button");
 let input = document.createElement("input");
 let result = document.createElement("p");
-let play = document.createElement("button");
 let back = document.createElement("button");
 let clear = document.createElement("button");
 
 let timer = document.querySelector("#time");
-let start = document.querySelector(".start");
 let timeLeft = 75;
 let timeInterval;
 
-let body = document.createElement("body");
-
 function press() {
+        let answers = ["3", "3", "4", "4", "4"];
         if (this.innerHTML !== "Start Quiz" && this.innerHTML !== "Play again?") {
             
             if (this.innerHTML.includes(answers[i])) {
@@ -52,25 +47,23 @@ function press() {
             else {
                 incorrect++;
                 result.innerHTML = "Incorrect!";
-                timeLeft = timeLeft - 15;
+                if (timeLeft > 15) {
+                    timeLeft = timeLeft - 15;
+                }
+                else {
+                    timeLeft = 0;
+                }
             }
             i++;
             footer.innerHTML = "";
             footer.appendChild(result);
         }
-        
-        // console.log(this.innerHTML);
-        // console.log(answers[i]);
-        // console.log("Correct: " + correct);
-        // console.log("Incorrect: " + incorrect);
 
     if (i < 5) {
         bold.innerHTML = "";
-        let question = document.createElement("h2");
+        let question = document.createElement("h1");
         question.innerHTML = fiveQuestions[i][0];
         bold.appendChild(question);
-        // console.log(question);
-
         content.innerHTML = "";
 
         for (j = 1; j <= 4; j++) {
@@ -80,7 +73,6 @@ function press() {
             content.appendChild(one);
             one.addEventListener("click", press);
         }
-        // console.log(div);
     }
     else {
         allDone();
@@ -94,56 +86,31 @@ function allDone() {
         let finish = document.createElement("h2");
         finish.innerHTML = "All done!";
         bold.appendChild(finish);
-        // console.log(finish);
 
         content.innerHTML = "";
         let score = document.createElement("h2");
         score.innerHTML = "Your final score is " + timeLeft + ".";
         content.appendChild(score);
-        // timeLeft = 0;
-        // console.log(score);
 
         div.innerHTML = "";
         content.appendChild(div);
-        let initials = document.createElement("h2");
+        let initials = document.createElement("p");
         initials.innerHTML = "Enter initials:";
         div.appendChild(initials);
-        // console.log(initials);
 
-        // input.setAttribute("type", "text");
         div.appendChild(input);
-        // console.log(input);
 
         submit.classList.add("submit");
         submit.innerHTML = "Submit";
         div.appendChild(submit);
-        // console.log(submit);
-
-        // footer.innerHTML = "";
-        result.innerHTML = "Correct: " + correct + " Incorrect: " + incorrect;
 }
 
-function submitIt() {
+submit.addEventListener("click", function() {
     let name = input.value;
-    scores.push([name, timeLeft, correct, incorrect]);
-    // console.log(scores[scores.length - 1]);
-    play.innerHTML = "Play again?";
-    play.classList.add("start");
-    play.classList.add("button");
-    bold.innerHTML = "";
-    
-    
-    // div.appendChild(play);
-
-    // back.innerHTML = "Back";
-    // div.appendChild(back);
-
-    i = 0;
-    correct = 0;
-    incorrect = 0;
+    scores.push([name, timeLeft]);
     highScores();
-    // console.log(i);
-}
+});
+
 
 function highScores() {
     clearInterval(timeInterval);
@@ -156,37 +123,28 @@ function highScores() {
     highScoresBtn.innerHTML = "";
     content.innerHTML = "";
     div.innerHTML = "";
-    content.appendChild(div);
-    // highScoresBtn.innerHTML = "Main menu";
-    // boldTitle.innerHTML = "High Scores";
-
     bold.innerHTML = "";
+
     let intro = document.createElement("h2");
     intro.innerHTML = "High Scores";
     bold.appendChild(intro);
 
     for (j in scores) {
         let hs = document.createElement("p");
-        hs.innerHTML = scores[j][0] + " - Score: " + scores[j][1] + " Correct: " + scores[j][2] + " Incorrect: " + scores[j][3];
-        div.appendChild(hs);
+        hs.innerHTML = scores[j][0] + " - " + scores[j][1];
+        content.appendChild(hs);
     }
-
-    let div2 = document.createElement("div");
-    div2.classList.add("div2");
-    div.appendChild(div2);
     
+    content.appendChild(div);
+
     back.innerHTML = "Go Back";
-    div2.appendChild(back);
+    div.appendChild(back);
 
     clear.innerHTML = "Clear Highscores";
-    div2.appendChild(clear);
-
-    // for (j in scores) {
-    //     console.log(scores[j]);
-    // }
+    div.appendChild(clear);
 }
 
-function goBack() {
+back.addEventListener("click", function() {
     bold.innerHTML = "";
     content.innerHTML = "";
     div.innerHTML = "";
@@ -203,68 +161,49 @@ function goBack() {
     directions.innerHTML = "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your scoreTime by ten seconds!";
     content.appendChild(directions);
 
-    button.innerHTML = "";
-    button.classList.add("start");
-    button.classList.add("button");
-    button.innerHTML = "Start Quiz";
     div.appendChild(button);
     content.appendChild(div);
-}
-
-function countdown() {
-    timer.innerHTML = "Time: " + timeLeft;
-
-        timeInterval = setInterval(function () {
-            timeLeft--;
-            timer.innerHTML = "Time: " + timeLeft;
-
-            // if (timeLeft >= 0) {
-                
-            // }
-            if (timeLeft === 0) {
-                clearInterval(timeInterval);
-                allDone();
-            }
-            
-        }, 1000);
-}
+});
 
 button.addEventListener("click", press);
-button.addEventListener("click", countdown);
 button.addEventListener("click", function() {
     timeLeft = 75;
     time.innerHTML = "Time: " + timeLeft;
     footer.innerHTML = "";
 })
+button.addEventListener("click", function() {
+    timer.innerHTML = "Time: " + timeLeft;
 
-submit.addEventListener("click", submitIt);
+    timeInterval = setInterval(function () {
+        if (timeLeft > 0) {
+            timeLeft--;
+            timer.innerHTML = "Time: " + timeLeft;
+        }
+        if (timeLeft === 0) {
+            allDone();
+        }
+    }, 1000);
+});
+
+
 highScoresBtn.addEventListener("click", highScores);
 
-// play.addEventListener("click", press);
-play.addEventListener("click", function() {
-    // press();
-    timeLeft = 75;
-    footer.innerHTML = "";
-    // countdown();
-})
-play.addEventListener("click", press);
-play.addEventListener("click", countdown);
 
-back.addEventListener("click", goBack);
+input.addEventListener("click", function() {
+    result.innerHTML = "Correct: " + correct + " Incorrect: " + incorrect;
+    // footer.innerHTML = "";
+})
 
 clear.addEventListener("click", function() {
     scores = [];
+    content.innerHTML = "";
 
-    div.innerHTML = "";
-
-    let div2 = document.createElement("div");
-    div2.classList.add("div2");
-    div.appendChild(div2);
+    let div = document.createElement("div");
+    content.appendChild(div);
 
     back.innerHTML = "Go Back";
-    div2.appendChild(back);
+    div.appendChild(back);
 
     clear.innerHTML = "Clear Highscores";
-    div2.appendChild(clear);
+    div.appendChild(clear);
 })
-
